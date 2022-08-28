45ca287256d20d67e8982d0d081579091f907758
Tenzin Norden <nordentenzin29@gmail.com>
2022-08-28 12:22:46 +0530

    sample base

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+// const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const functions = require("firebase-functions");
+const express = require("express");
+const app = express();
+app.get("*", (req, res) => {
+  res.send("Hello from the API");
+});
+exports.api = functions.https.onRequest(app);
